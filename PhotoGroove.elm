520be417ec2f6ef38e8427433d31cb6bffe39cce
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-03-19 15:00:38 -0700

    Add REDME, apply elm-format first time

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -3,34 +3,41 @@ module PhotoGroove exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
 urlPrefix =
-  "http://elm-in-action.com/"
+    "http://elm-in-action.com/"
+
 
 viewThumbnail selectedUrl thumbnail =
-  if selectedUrl == thumbnail.url then
-    img
-      [ src (urlPrefix ++ thumbnail.url)
-      , class "selected"
-      ]
-      []
-  else
-    img [ src (urlPrefix ++ thumbnail.url) ] []
+    if selectedUrl == thumbnail.url then
+        img
+            [ src (urlPrefix ++ thumbnail.url)
+            , class "selected"
+            ]
+            []
+    else
+        img [ src (urlPrefix ++ thumbnail.url) ] []
+
 
 view model =
-  div [ class "content"]
-    [ h1 [] [ text "Photo Groove"]
-    , div [ id "thumbnails"]
-      (List.map (viewThumbnail model.selectedUrl) model.photos)
-    ]
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        ]
+
 
 initialModel =
-  { photos =
-    [ { url = "1.jpeg"}
-    , { url = "2.jpeg"}
-    , { url = "3.jpeg"}
-    ]
-  , selectedUrl = "1.jpeg" -- select the first photo by default
-  }
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl =
+        "1.jpeg"
+        -- select the first photo by default
+    }
+
 
 main =
-  view initialModel
+    view initialModel
---
+++ README.md
@@ -0,0 +1,10 @@
+Elm in Action book
+https://www.manning.com/books/elm-in-action
+
+### How to run
+
+```sh
+git clone
+elm-live PhotoGroove.elm --output=elm.js # use https://github.com/tomekwi/elm-live
+open http://localhost:8000/
+```
