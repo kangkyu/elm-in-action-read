62d4b4044eab995d63ccda21c6b13657f76c6882
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-05-19 15:58:23 -0700

    Add onClick event and type annotations to functions

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -2,16 +2,20 @@ module PhotoGroove exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+viewThumbnail : String -> Photo -> Html { data : String, operation : String }
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
         ]
         []
 
@@ -29,17 +33,36 @@ view model =
         ]
 
 
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo, selectedUrl : String }
+
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
         , { url = "2.jpeg" }
         , { url = "3.jpeg" }
         ]
-    , selectedUrl =
-        "1.jpeg"
-        -- select the first photo by default
+    , selectedUrl = "1.jpeg" {- select the first photo by default -}
     }
 
 
+update : { data : String, operation : String } -> Model -> Model
+update msg model =
+    if msg.operation == "SELECT_PHOTO" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
+
 main =
-    view initialModel
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
